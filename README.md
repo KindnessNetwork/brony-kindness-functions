c920956f88396db9cdaa3b495c345b74d156b6aa
LinuxPony <sonic.ert@gmail.com>
2023-03-13 11:24:41 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Brony Kindness Functions
+
+WordPress Plugin with miscellanies functions and shortcodes used by the Brony Kindness Network.
+
+Plugin Licensed under the GPL.
+
+Initial Commit. Will tidy up later. 
---
+++ brony-kindness-functions.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package Brony_Kindness_Functions
+ * @version 1.7.2
+ */
+/*
+Plugin Name: Brony Kindness Functions
+Plugin URI: https://github.com/KindnessNetwork/brony-kindness-functions
+Author: Brony Kindness Network, LinuxPony
+Author URI: https://bronykindness.net/
+Description: WordPress Plugin with miscellanies functions and shortcodes used by the Brony Kindness Network.
+Version: 0.1.0
+License: GNU General Public License
+License URI: https://www.gnu.org/licenses/gpl.html
+Text Domain: bkn
+*/
+
+define('BKN_PLUGIN_VER', '0.1.0');
+
+class Bkn_Functions {
+    public static function init() {
+        add_shortcode('bkn-staff-image-grid', [static::class, 'render_staff_image_grid']);
+        add_action('init', [static::class, 'register_includes']);
+    }
+    public static function register_includes() {
+        wp_register_style("bkn-functions-style", plugins_url("functions.css", __FILE__), [], BKN_PLUGIN_VER);
+    }
+    public static function render_staff_image_grid($scodeArgs) {
+        if(!is_plugin_active("staff-list/staff-list.php")){
+            return sprintf('<p>%s</p>', __("The Staff List plugin is required, but is either not installed or not enabled.", "bkn"));
+        }
+
+        $placeholder_image = get_post_meta($scodeArgs['id'], '_pImgIDL', true);
+        if(empty($placeholder_image)) {
+            return sprintf('<p>%s</p>', __("Please Provide a valid staff template id, or make sure a placeholder image is specified.", "bkn"));
+        }
+        $default_image = @wp_get_attachment_image($placeholder_image, 'medium', false, ['class' => 'bkn-staff-picture']);
+        unset($placeholder_image);
+
+        $postIDs = abcfsl_db_all_staff_ids_sorted($scodeArgs['id'], [
+            "scodeOrder" => "",
+            "sortType" => "M",
+            "dSort" => "",
+            "dSortOrder" => "",
+            "cSort" => "",
+            "cSortOrder" => "",
+            "scodeCat" => $scodeArgs['category'] ?? "",
+            "scodeCatExcl" => "",
+            "hiddenFields" => "0",
+            "hiddenRecords" => "0",
+            "privateFields" => "0"
+        ]);
+
+        if(count($postIDs) <= 0) {
+            return sprintf('<p>%s</p>', __("No staff to display.", "bkn"));
+        }
+
+        $items = '';
+
+        foreach ($postIDs as $id) {
+            $post_meta = get_post_meta($id);
+            $image = @wp_get_attachment_image($post_meta['_imgIDL'][0], 'medium', false, ['class' => 'bkn-staff-picture']);
+            if(empty($image)) {
+                $image = $default_image;
+            }
+            /** @noinspection HtmlUnknownTarget */
+            $link = sprintf('<div class="bkn-staff-link"><a href="%s">%s</a></div>',
+                $post_meta['_icon2Url_F5'][0], $post_meta['_mp1_F1'][0]);
+            $items .= sprintf('<div class="bkn-staff">%s</div>',$image . $link);
+        }
+
+        wp_enqueue_style("bkn-functions-style");
+        return sprintf('<div class="bkn-staff-grid">%s</div>', $items);
+    }
+}
+
+Bkn_Functions::init();
+
